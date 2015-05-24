2f62d3b0da537b5074fc6b3c8f61a60371866655
Grandman <root@grandman73.ru>
2015-05-24 14:21:20 +0300

    crud tasks, association, tests

--- spec/requests/api/projects_spec.rb
+++ spec/requests/api/projects_spec.rb
@@ -20,17 +20,23 @@ RSpec.describe 'projects', type: :request do
       post "/api/projects/", project: project_attributes
       expect(response).to be_success
     end
+
     it 'error if not valid' do
       post "/api/projects/", project: { test: 'test' }
       expect(response).to have_http_status(422)
     end
   end
   context 'project update' do
-    let(:project) { create :project }
+    let!(:project) { create :project }
     let(:project_attributes) { attributes_for :project }
     it 'success if valid project' do
       patch "/api/projects/1", project: project_attributes
       expect(response).to be_success
+    end
+
+    it 'error if not valid project params' do
+      patch "/api/projects/1", project: {title: nil}
+      expect(response).to have_http_status(422)
     end
   end
 end
